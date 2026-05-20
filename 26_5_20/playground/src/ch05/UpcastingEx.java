f3a467e06a5ea0ce9f7aae6997e4499a3af4a83a
gogogomT <wndwnddoq@gmail.com>
2026-05-21 07:52:36 +0900

    .

--- 26_5_20/playground/src/ch05/UpcastingEx.java
+++ 26_5_20/playground/src/ch05/UpcastingEx.java
@@ -1,4 +1,29 @@
 package ch05;
 
+class Person {
+    String name;
+    int id;
+
+    public Person(String name) {
+        this.name = name;
+    }
+}
+
+class Student extends Person {
+    String grade;
+    String department;
+
+    public Student(String name) {
+        super(name);
+    }
+}
+
 public class UpcastingEx {
+    public static void main(String[] args) {
+        Person p;
+        Student s = new Student("이재문");
+        p=s;
+
+        System.out.println(p.name);
+    }
 }
